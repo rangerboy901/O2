2ce047c1f7e6cac9012add658db2460164f9d363
Joseph DeWeese <rangerboy901@icloud.com>
2022-09-18 15:36:18 -0500

    Initial Commit

---
+++ O2/O2App.swift
@@ -0,0 +1,17 @@
+//
+//  O2App.swift
+//  O2
+//
+//  Created by Joseph Wil;liam DeWeese on 9/18/22.
+//
+
+import SwiftUI
+
+@main
+struct O2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
